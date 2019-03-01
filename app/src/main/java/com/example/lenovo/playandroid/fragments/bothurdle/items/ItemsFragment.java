58740bb67ae654f4a93f0d89da2accd19d82997c
MrYangXu <2395693796@qq.com>
2019-03-01 19:47:52 +0800

    上传第N次

--- app/src/main/java/com/example/lenovo/playandroid/fragments/bothurdle/items/ItemsFragment.java
+++ app/src/main/java/com/example/lenovo/playandroid/fragments/bothurdle/items/ItemsFragment.java
@@ -20,8 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
